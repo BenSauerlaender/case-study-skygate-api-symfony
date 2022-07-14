9925a6f6ce8956dc3107a4e45e58321ca5b07975
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-07-14 13:51:40 +0200

    layout register route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,9 +8,23 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): JsonResponse
+    #[Route('/register', name: 'user_register', methods: ['GET'])]
+    public function register(): JsonResponse
     {
+
+        //validate
+        //common
+        //role
+        //email
+
+        //hash password
+        //generate veri-code
+        //insert
+
+        //send email
+
+        //return created
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/UserController.php',
