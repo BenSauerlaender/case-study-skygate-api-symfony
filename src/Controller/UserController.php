bb503b8000f5fd555ba343b1ccf0c4eadf195e96
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-07-15 13:22:31 +0200

    add validation error exception

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Role;
 use App\Entity\User;
+use App\Exceptions\ValidationError;
 use App\Service\CodeGenerator;
 use App\Service\PasswordHasher;
 use Doctrine\Persistence\ManagerRegistry;
@@ -45,14 +46,7 @@ class UserController extends AbstractController
         $errors = $validator->validate($user);
 
         if (count($errors) > 0) {
-            /*
-             * Uses a __toString method on the $errors variable which is a
-             * ConstraintViolationList object. This gives us a nice string
-             * for debugging.
-             */
-            $errorsString = (string) $errors;
-
-            return new JsonResponse($errorsString);
+            throw new ValidationError($errors);
         }
 
         $doctrine
